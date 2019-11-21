8544413b08d9279b20109cd35834a72c6efeaa01
Soreine <hello@soreine.dev>
2019-11-21 17:09:01 +0100

    Implemented all select and input logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import Select from "react-select";
+import React, { useState } from "react";
 import "./App.css";
 
 // type Time = number;
@@ -17,40 +16,66 @@ import "./App.css";
 
 // function timeSaved(avoidedTask: Task): Time {}
 
-type TimeSpentUnit = "minute" | "hour" | "day" | "week" | "month" | "year";
-
-// function Select<T extends string>(props: {
-//   onChange: (newValue: T) => void;
-//   values: Array<[T, string]>;
-// }): React.ReactNode {
-//   return (
-//     <select
-//       onChange={event => {
-//         const newValue = (event.target.value as any) as T;
-//         props.onChange(newValue);
-//       }}
-//     >
-//       {props.values.map(([value, text]) => (
-//         <option value={value}>{text}</option>
-//       ))}
-//     </select>
-//   );
-// }
+class Select<T extends string> extends React.Component<{
+  value: T;
+  onChange: (newValue: T) => void;
+  options: Array<{ value: T; label: string }>;
+}> {
+  render(): React.ReactNode {
+    const { onChange, options, value } = this.props;
+    return (
+      <select
+        value={value}
+        onChange={event => {
+          const newValue = (event.target.value as any) as T;
+          onChange(newValue);
+        }}
+      >
+        {options.map(option => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+}
 
 function NumberInput(props: {
-  value: number;
-  onChange: (newValue: number) => void;
+  value: number | null;
+  onChange: (newValue: number | null) => void;
 }) {
   return (
     <input
-      value={props.value}
-      onChange={e => props.onChange(parseInt(e.target.value))}
+      type="number"
+      min="0"
+      value={props.value === null ? "" : props.value}
+      onChange={e => {
+        if (!e.target.value) {
+          return props.onChange(null);
+        }
+
+        const value = Math.max(0, parseInt(e.target.value, 10));
+        props.onChange(0);
+      }}
     />
   );
 }
 
-type TimeSpentUnitOption = { value: TimeSpentUnit; label: string };
-const TIME_SPENT_OPTIONS: TimeSpentUnitOption[] = [
+type TimeUnit =
+  | "second"
+  | "minute"
+  | "hour"
+  | "day"
+  | "week"
+  | "month"
+  | "year";
+
+type FrequencyUnit = "daily" | "weekly" | "monthly" | "yearly";
+
+type TimeUnitOption = { value: TimeUnit; label: string };
+
+const TIME_SPENT_UNIT_OPTIONS: Array<TimeUnitOption> = [
   { value: "minute", label: "minutes" },
   { value: "hour", label: "hour" },
   { value: "day", label: "day" },
@@ -58,72 +83,111 @@ const TIME_SPENT_OPTIONS: TimeSpentUnitOption[] = [
   { value: "month", label: "month" },
   { value: "year", label: "year" }
 ];
-const DEFAULT_TIME_SPENT_OPTION = TIME_SPENT_OPTIONS[1];
+const DEFAULT_TIME_SPENT_UNIT = TIME_SPENT_UNIT_OPTIONS[1].value;
+
+const TIME_SHAVED_UNIT_OPTIONS: Array<TimeUnitOption> = [
+  { value: "second", label: "second" },
+  { value: "minute", label: "minute" },
+  { value: "hour", label: "hour" },
+  { value: "day", label: "day" },
+  { value: "week", label: "week" },
+  { value: "month", label: "month" }
+];
+const DEFAULT_TIME_SHAVED_UNIT = TIME_SHAVED_UNIT_OPTIONS[1].value;
+
+const TASK_LIFETIME_UNIT_OPTIONS: Array<TimeUnitOption> = [
+  { value: "day", label: "day" },
+  { value: "week", label: "week" },
+  { value: "month", label: "month" },
+  { value: "year", label: "year" }
+];
+const DEFAULT_TASK_LIFETIME_UNIT = TASK_LIFETIME_UNIT_OPTIONS[1].value;
+
+const TASK_FREQUENCY_UNIT_OPTIONS: Array<{
+  value: FrequencyUnit;
+  label: string;
+}> = [
+  { value: "daily", label: "a day" },
+  { value: "weekly", label: "a week" },
+  { value: "monthly", label: "a month" },
+  { value: "yearly", label: "a year" }
+];
+const DEFAULT_TASK_FREQUENCY_UNIT = TASK_FREQUENCY_UNIT_OPTIONS[0].value;
+
+function useUnitValue<U, V>(defaultValue: V, defaultUnit: U) {
+  const [value, setValue] = React.useState<V | null>(defaultValue);
+  const [unit, setUnit] = React.useState<U>(defaultUnit);
+
+  return {
+    value: {
+      current: value,
+      set: setValue
+    },
+    unit: {
+      current: unit,
+      set: setUnit
+    }
+  };
+}
 
 const App: React.FC = () => {
-  const [timeSpentValue, setTimeSpentValue] = React.useState<number>(1);
-  const [timeSpentUnitOption, setTimeSpentUnitOption] = React.useState<
-    TimeSpentUnitOption
-  >(DEFAULT_TIME_SPENT_OPTION);
+  const timeSpent = useUnitValue(1, DEFAULT_TIME_SPENT_UNIT);
+  const timeShaved = useUnitValue(1, DEFAULT_TIME_SHAVED_UNIT);
+  const taskLifetime = useUnitValue(2, DEFAULT_TASK_LIFETIME_UNIT);
+  const taskFrequency = useUnitValue(10, DEFAULT_TASK_FREQUENCY_UNIT);
+  const [daysPerWeek, setDaysPerWeek] = React.useState<number | null>(5);
 
   const timeSaved = 30;
 
   return (
     <div className="App">
       <div>
-        {"If I spend "}
-        <NumberInput value={timeSpentValue} onChange={setTimeSpentValue} />
-        <Select
-          value={timeSpentUnitOption}
-          onChange={(
-            selected?: TimeSpentUnitOption | TimeSpentUnitOption[] | void
-          ) => {
-            return;
-          }}
-          options={[
-            { value: "minute", label: "minutes" },
-            { value: "hour", label: "hour" },
-            { value: "day", label: "day" },
-            { value: "week", label: "week" },
-            { value: "month", label: "month" },
-            { value: "year", label: "year" }
-          ]}
+        {"By spending "}
+        <NumberInput
+          value={timeSpent.value.current}
+          onChange={timeSpent.value.set}
         />
-        <select onChange={setTimeSpentUnit}></select>
+        <Select
+          value={timeSpent.unit.current}
+          onChange={timeSpent.unit.set}
+          options={TIME_SPENT_UNIT_OPTIONS}
+        />
         {" to shave off "}
-        <input value={1} />
-        <select>
-          <option value="second">second</option>
-          <option value="minute" selected>
-            minute
-          </option>
-          <option value="hour">hour</option>
-        </select>
+        <NumberInput
+          value={timeShaved.value.current}
+          onChange={timeShaved.value.set}
+        />
+        <Select
+          value={timeShaved.unit.current}
+          onChange={timeShaved.unit.set}
+          options={TIME_SHAVED_UNIT_OPTIONS}
+        />
         <br />
         {" of a task that I will do for "}
-        <input placeholder="How long will you do the task" value={2} />
-        <select>
-          <option value="day">day</option>
-          <option value="week">week</option>
-          <option value="month">month</option>
-          <option value="year" selected>
-            year
-          </option>
-        </select>
-        {", "} <br />
-        <input placeholder="How often you do the task" value={10} />
+        <NumberInput
+          value={taskLifetime.value.current}
+          onChange={taskLifetime.value.set}
+        />
+        <Select
+          value={taskLifetime.unit.current}
+          onChange={taskLifetime.unit.set}
+          options={TASK_LIFETIME_UNIT_OPTIONS}
+        />
+        {", "}
+        <br />
+        <NumberInput
+          value={taskFrequency.value.current}
+          onChange={taskFrequency.value.set}
+        />
         {" times "}
-        <select>
-          <option value="daily" selected>
-            a day
-          </option>
-          <option value="weekly">a week</option>
-          <option value="monthly">a month</option>
-          <option value="yearly">a year</option>
-        </select>
+        <Select
+          value={taskFrequency.unit.current}
+          onChange={taskFrequency.unit.set}
+          options={TASK_FREQUENCY_UNIT_OPTIONS}
+        />
         <br />
         {" and "}
-        <input placeholder="How many days in a week" value={5} />
+        <NumberInput value={daysPerWeek} onChange={setDaysPerWeek} />
         {" days per week."}
       </div>
       <div>
