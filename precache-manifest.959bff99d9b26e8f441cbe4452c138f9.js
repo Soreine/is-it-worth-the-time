99200d61ec0f4ce1831c76ac8682dc3998c7abe6
Soreine <hello@soreine.dev>
2019-11-24 23:19:22 +0100

    Updates

---
+++ precache-manifest.959bff99d9b26e8f441cbe4452c138f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea5babd832931c86a5c04e34f2f35e5",
+    "url": "/is-it-worth-the-time/index.html"
+  },
+  {
+    "revision": "0ec96a46f5bc3621f369",
+    "url": "/is-it-worth-the-time/static/css/main.7c4e51a4.chunk.css"
+  },
+  {
+    "revision": "382a94432788fc70fd26",
+    "url": "/is-it-worth-the-time/static/js/2.9c8f38a0.chunk.js"
+  },
+  {
+    "revision": "0ec96a46f5bc3621f369",
+    "url": "/is-it-worth-the-time/static/js/main.1f1ddaf3.chunk.js"
+  },
+  {
+    "revision": "e3f07cb4bd1753877236",
+    "url": "/is-it-worth-the-time/static/js/runtime-main.2df386cd.js"
+  }
+]);
